9a91a5b131f2141fcf90a8709be5ae60e63ea29a
shredder42 <atbinder42@gmail.com>
2024-12-05 16:27:13 -0600

    htmlnode

---
+++ src/htmlnode.py
@@ -0,0 +1,22 @@
+class HTMLNode:
+    def __init__(self, tag=None, value=None, children=None, props=None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+
+    def to_html(self):
+        raise NotImplementedError("to_html method not implemented")
+    
+    def props_to_html(self):
+        if self.props:
+            html = ''
+            for k, v in self.props.items():
+                html += f' {k}="{v}"'
+            return html
+        else:
+            return ''
+
+    def __repr__(self):
+        return f"HTMLNode(Tag: {self.tag}; Value: {self.value}; Children: {self.children}; Props: {self.props})"
+    
