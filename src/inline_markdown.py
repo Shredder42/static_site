dca546a32d6e1946cfadc560a6d3d5da251cda2e
shredder42 <atbinder42@gmail.com>
2024-12-11 11:58:16 -0600

    text to textnodes

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -85,3 +85,11 @@ def split_nodes_link(old_nodes):
             new_nodes.append(TextNode(original_text, TextType.TEXT))
     return new_nodes
 
+def text_to_textnodes(text):
+    node_list = [TextNode(text, TextType.TEXT)]
+    node_list = split_nodes_delimiter(node_list, '**', TextType.BOLD)
+    node_list = split_nodes_delimiter(node_list, '*', TextType.ITALIC)
+    node_list = split_nodes_delimiter(node_list, '`', TextType.CODE)
+    node_list = split_nodes_image(node_list)
+    node_list = split_nodes_link(node_list)
+    return node_list
--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -7,6 +7,7 @@ from inline_markdown import (
     split_nodes_link,
     extract_markdown_links,
     extract_markdown_images,
+    text_to_textnodes
 )
 
 class TestInlineMarkdown(unittest.TestCase):
@@ -130,4 +131,24 @@ class TestInlineMarkdown(unittest.TestCase):
     TextNode(
         "to youtube", TextType.LINK, "https://www.youtube.com/@bootdotdev"
     ),
+    ])
+        
+    def text_text_to_textnodes_all(self):
+        text = "This is **text** with an *italic* word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        self.assertEqual(text_to_textnodes(text), [
+    TextNode("This is ", TextType.TEXT),
+    TextNode("text", TextType.BOLD),
+    TextNode(" with an ", TextType.TEXT),
+    TextNode("italic", TextType.ITALIC),
+    TextNode(" word and a ", TextType.TEXT),
+    TextNode("code block", TextType.CODE),
+    TextNode(" and an ", TextType.TEXT),
+    TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+    TextNode(" and a ", TextType.TEXT),
+    TextNode("link", TextType.LINK, "https://boot.dev"),
+    ])
+    def text_text_to_textnodes_none(self):
+        text = "This is text with nothing in it"
+        self.assertEqual(text_to_textnodes(text), [
+    TextNode("This is text with nothing in it", TextType.TEXT),
     ])
