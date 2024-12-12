f1f6d28ecf207e9820ac717399610a34fe43b2c4
shredder42 <atbinder42@gmail.com>
2024-12-12 12:07:54 -0600

    block types

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,27 +1,108 @@
+block_type_paragraph = "paragraph"
+block_type_heading = "heading"
+block_type_code = "code"
+block_type_quote = "quote"
+block_type_olist = "ordered_list"
+block_type_ulist = "unordered_list"
+
 def markdown_to_blocks(markdown):
     blocks = markdown.split('\n\n')
     filtered_blocks = []
     for block in blocks:
+        # i reversed the block.strip() line belwo with the if statement below that from the boot_dev solution
+        # it was failing one of the tests written the way in the solution
+        block = block.strip()    
         if block == "":
             continue
-        block = block.strip()
         filtered_blocks.append(block)
     return filtered_blocks
 
+def block_to_block_type(block):
+    lines = block.split("\n")
+
+    if block.startswith(("# ", "## ", "### ", "#### ", "##### ", "###### ")):
+        return block_type_heading
+    if len(lines) > 1 and lines[0].startswith("```") and lines[-1].endswith("```"):
+        return block_type_code
+    if block.startswith(">"):
+        for line in lines:
+            if not line.startswith(">"):
+                return block_type_paragraph
+        return block_type_quote
+    if block.startswith("* "):
+        for line in lines:
+            if not line.startswith("* "):
+                return block_type_paragraph
+        return block_type_ulist
+    if block.startswith("_ "):
+        for line in lines:
+            if not line.startswith("_ "):
+                return block_type_paragraph
+        return block_type_ulist
+    if block.startswith("1. "):
+        i = 1
+        for line in lines:
+            if not line.startswith(f"{i}. "):
+                return block_type_paragraph
+            i += 1
+        return block_type_olist
+    return block_type_paragraph
 
 
 
+'''
+Alex written fuctions
 
+def markdown_to_blocks(markdown):
+    final_blocks = []
+    blocks = markdown.split('\n\n')
+    for block in blocks:
+        if block and not block.isspace():
+            stripped_block = block.strip(' ')
+            if stripped_block.startswith('\n'):
+                stripped_block = stripped_block.replace('\n', '')
+            final_blocks.append(stripped_block)
+    return final_blocks
 
-# Alex written fuctions
+    
+def block_to_block_type(block):
+    lines = block.split('\n')
 
-# def markdown_to_blocks(markdown):
-#     final_blocks = []
-#     blocks = markdown.split('\n\n')
-#     for block in blocks:
-#         if block and not block.isspace():
-#             stripped_block = block.strip(' ')
-#             if stripped_block.startswith('\n'):
-#                 stripped_block = stripped_block.replace('\n', '')
-#             final_blocks.append(stripped_block)
-#     return final_blocks
+    available_headings = ['#' * i + ' ' for i in range(1,7)]
+    heading_block = block
+    heading_string = ''
+    for value in heading_block:
+        if value == '#':
+            heading_string += value
+            heading_block = heading_block[1:]
+        elif value == ' ':
+            heading_string += value
+            break
+        else:
+            break
+    if heading_string in available_headings:
+        return 'heading'
+    
+    if block.startswith('```') and block.endswith('```'):
+        return 'code'
+    
+    for line in lines:
+        if not line.startswith('>'):
+            break
+    else:
+        return 'quote'
+    
+    for line in lines:
+        if not (line.startswith('* ') or line.startswith('_ ')):
+            break
+    else:
+        return 'unordered list'
+    
+    for line in lines:
+        if not (line.startswith(f'{lines.index(line) + 1}. ')):
+            break
+    else:
+        return 'ordered list'
+    
+    return 'paragraph'
+'''
--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -1,6 +1,6 @@
 import unittest
 
-from markdown_blocks import markdown_to_blocks
+from markdown_blocks import markdown_to_blocks, block_to_block_type
 
 class TestBlockMarkdown(unittest.TestCase):
     def test_markdown_to_blocks(self):
@@ -45,4 +45,54 @@ class TestBlockMarkdown(unittest.TestCase):
 # * This is another list item'''
         self.assertEqual(markdown_to_blocks(text), ['# This is a heading', '# This is a paragraph of text. It has some **bold** and *italic* words inside of it.', '# * This is the first list item in a list block\n# * This is a list item\n# * This is another list item'])
 
+    def test_block_to_block_type_heading(self):
+        text = '''## ghost'''
+    def test_block_to_block_type_no_pound_heading(self):
+        text = ''' ghost'''
+        self.assertEqual(block_to_block_type(text), 'paragraph')
+    def test_block_to_block_type_too_many_pound_signs(self):
+        text = '''####### ghost'''
+        self.assertEqual(block_to_block_type(text), 'paragraph')
+    def test_block_to_block_type_code(self):
+        text = '''```
+ghost
+```'''
+        self.assertEqual(block_to_block_type(text), 'code')
+    def test_block_to_block_type_code_with_heading(self):
+        text = '''```
+## ghost
+```'''
+        self.assertEqual(block_to_block_type(text), 'code')
+    def test_block_to_block_type_quote(self):
+        text = '''>ghost'''
+        self.assertEqual(block_to_block_type(text), 'quote')
+    def test_block_to_block_type_multi_line_quote(self):
+        text = '''>ghost
+>alex'''
+        self.assertEqual(block_to_block_type(text), 'quote')
+    def test_block_to_block_type_incorrect_quote(self):
+        text = '''>ghost
+* alex'''
+        self.assertEqual(block_to_block_type(text), 'paragraph')
+    def test_block_to_block_type_uo_list(self):
+        text = '''* ghost
+* alex'''
+        self.assertEqual(block_to_block_type(text), 'unordered_list')
+    def test_block_to_block_type_o_list(self):
+        text = '''1. ghost
+2. alex'''
+        self.assertEqual(block_to_block_type(text), 'ordered_list')
+    def test_block_to_block_type_out_of_order_list(self):
+        text = '''1. ghost
+2. alex
+4. hannah'''
+        self.assertEqual(block_to_block_type(text), 'paragraph')
+    def test_block_to_block_type_mixed_list(self):
+        text = '''1. ghost
+2. alex
+_ hannah'''
+        self.assertEqual(block_to_block_type(text), 'paragraph')
+    def test_block_to_block_type_paragraph(self):
+        text = '''ghost'''
+        self.assertEqual(block_to_block_type(text), 'paragraph')
 
